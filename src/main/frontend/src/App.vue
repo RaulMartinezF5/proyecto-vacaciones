43af4a231534bdbf838f07e84d31e2bfc51d8b10
Your Name <iyanperezmarcos@gmail.com>
2023-03-30 15:18:27 +0200

    Request component[in progress]

--- src/main/frontend/src/App.vue
+++ src/main/frontend/src/App.vue
@@ -1,22 +1,23 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
 import FormComponent from "./components/FormComponent.vue";
+import Request from "./components/Request.vue";
 
 </script>
 
 <template>
   <header>
     <nav>
-      <RouterLink to="/">Home</RouterLink> 
-      <RouterLink to="/about">About</RouterLink>
+<!--       <RouterLink to="/">Home</RouterLink> 
+      <RouterLink to="/about">About</RouterLink> -->
     </nav>
   </header>
-
+<Request/>
  
 
   <main>
 
-  <RouterView />
+  <!-- <RouterView /> -->
   </main>
 </template>
 
---
+++ src/main/frontend/src/components/Request.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from 'vue';
+
+
+    const remainingDays = ref("7")
+
+</script>
+
+<template>
+    <div class="container">
+        <v-text-field color="#FF4700" label="Start date" type="date" v-model="firstDay"> </v-text-field>
+        <v-text-field color="#FF4700" label="End date" type="date" v-model="lastDay"> <v-icon icon="mdi-vuetify"></v-icon></v-text-field>
+        <v-textarea class="textarea" color="#FF4700" label="Description" type="date" v-model="lastDay"> </v-textarea>
+        <p>Dias restantes de vacaciones: {{ remainingDays }}</p>
+        
+    </div>
+
+</template>
+
+<style lang="scss" scoped>
+    
+    .container {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        grid-gap: 2%;
+    }
+    .textarea{
+        grid-column: 1 / span 2;
+    }
+</style>
