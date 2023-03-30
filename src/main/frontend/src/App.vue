e8f6cc416eb3066336b3ce7b80907c49e4d2e5ff
blake S <design.harls@icloud.com>
2023-03-30 14:30:00 +0200

    card de historial de solicitudes

--- src/main/frontend/src/App.vue
+++ src/main/frontend/src/App.vue
@@ -1,17 +1,20 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
+import CardHistorial from "./components/CardHistorial.vue";
+import LoginView from "./views/LoginView.vue";
 
 
 </script>
 
 <template>
-  <header>
+  <!-- <header>
     <nav>
       <RouterLink to="/">Home</RouterLink> 
       <RouterLink to="/about">About</RouterLink>
     </nav>
-  </header>
- 
+  </header> -->
+  <CardHistorial/>
+
 
   <main>
 
--- src/main/frontend/src/components/CardHistorial.vue
+++ src/main/frontend/src/components/CardHistorial.vue
@@ -1,21 +1,24 @@
 <script setup>
+import { ref,defineProps } from "vue";
+
+const props = defineProps({
+    Request :Object,
+})
 </script>
 
 <template>
-    <v-card
-      class="mx-auto"
-      width="400"
-      prepend-icon="mdi-home"
-    >
-      <template v-slot:title>
-        This is a title
-      </template>
-  
+  <div class="d-flex justify-center  flex-row aligth-center">
+    <v-card width="600">
       <v-card-text>
-        This is content
+        <div><v-card-title :="props.name">Andrés P.</v-card-title></div>
+        <p class="font-weight-medium  " :="props.date">12/12/12-13/05/21</p>
+        <p class="text-h6 font-weight-bold" :="props.days">Dias solicitados: 15</p>
+        <p class="text-h6 font-weight-bold" :="props.checked">Revisado por: Iyan</p>
+        <p class="text-h6 font-weight-bold" :="props.name_school">Nombre de la escuela</p>
+        <div class="text--primary" :="description"> well meaning and kindly "a benevolent smile"</div>
       </v-card-text>
     </v-card>
-  </template>
-
+  </div>
+</template>
 
 <style lang="scss" scoped></style>
