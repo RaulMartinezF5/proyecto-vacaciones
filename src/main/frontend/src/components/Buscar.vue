fe4830af36c8f442f13bbaf26f233fdb05934d94
RaulMartinezF5 <raulmartinezalvarez1980@gmail.com>
2023-03-31 15:40:09 +0200

    finalizado

---
+++ src/main/frontend/src/components/Buscar.vue
@@ -0,0 +1,33 @@
+<script setup>
+
+</script>
+
+<template>
+   <v-form>
+   <input type="text" v-model="searchTerm">
+        <v-btn @click="validate">
+          BUSCAR
+        </v-btn>
+</v-form> 
+
+</template>
+
+<style lang="scss" scoped>
+@use "../assets/scss/variables.scss" as c;
+
+input{
+width: 80vw;
+height: 2vw;
+border-color: map-get($map: c.$colors, $key: "Black");
+border: 2px solid;
+margin-right: 1rem;
+margin-left: 5vw;
+}
+.v-btn{
+   width: 10vw;
+   background-color:  map-get($map: c.$colors, $key: "Orange");
+    color:white;
+    border-radius: 10px;
+    margin-left: 1rem;
+}
+</style>
--- src/main/frontend/src/views/LoginView.vue
+++ src/main/frontend/src/views/LoginView.vue
@@ -1,10 +1,9 @@
 <script setup>
-import Header from "../components/Header.vue"
 import Login from "../components/Login.vue";
 </script>
 
 <template>
-  <Header></Header>
+
   <Login />
 </template>
 
