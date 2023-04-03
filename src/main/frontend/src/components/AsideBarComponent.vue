84232a4b2e4c9154811a13b710eb6771be4c6be2
David Daza <drdaza18@gmail.com>
2023-04-03 16:07:52 +0200

    create asideBar basics and refactor buttonComponent

---
+++ src/main/frontend/src/components/AsideBarComponent.vue
@@ -0,0 +1,54 @@
+<script setup>
+import ButtonComponent from './ButtonComponent.vue';
+import { computed, ref } from 'vue';
+
+const test = ref('ROLE_RESPONSABLE')
+
+const showRole = computed(()=>{
+    if(test.value == 'ROLE_USER') return 'EMPLEADO'
+    if(test.value == 'ROLE_RESPONSABLE') return 'RESPONSABLE'
+    if(test.value == 'ROLE_ADMIN') return 'ADMIN'
+}) 
+
+</script>
+<template>
+        <aside>
+            <div class="image-zone"></div>
+
+            <h1>{{ showRole }}</h1>
+
+            <div v-if="test == 'ROLE_USER'" class="user-info-zone">
+                <h1>Nombre</h1>
+                <h1>Escuela</h1>
+                <h1>Fechas de alta</h1>
+                <h1>Cargo</h1>
+            </div>
+
+            <div class="navigation-zone">
+                <div v-if="test == 'ROLE_USER'" class="navigation">
+                    <ButtonComponent :button="'EMPLEADO'" />
+                </div>
+                <div v-if="test == 'ROLE_RESPONSABLE'" class="navigation">
+                    <ButtonComponent :button="'EMPLEADO'" />
+                    <ButtonComponent :button="'RESPONSABLE'" />
+                </div>
+                <div v-if="test == 'ROLE_ADMIN'" class="navigation">
+                    <ButtonComponent :button="'CREAR USUARIO'" />
+                    <ButtonComponent :button="'PETICIONES'" />
+                    <ButtonComponent :button="'INFORMES'" />
+                    <ButtonComponent :button="'USUARIOS'" />
+                </div>
+            </div>
+        </aside>
+</template>
+<style lang="scss" scoped>
+@use '../assets/scss/mixins.scss' as *;
+aside{
+    width: 100%;
+    height: 100%;
+    @include gridDisplay(10,1);
+    .navigation-zone{
+        width: 100%;
+    }
+}
+</style>
--- src/main/frontend/src/layouts/LayoutSession.vue
+++ src/main/frontend/src/layouts/LayoutSession.vue
@@ -1,8 +1,26 @@
 <script setup>
 import { RouterView } from 'vue-router';
+import AsideBarComponent from '../components/AsideBarComponent.vue';
 </script>
 <template>
-    <!-- aquí deben ir nuestros componentes que se repitan en la session iniciada -->
-<RouterView/>
-
+    <main>
+        <div class="aside-space">
+            <AsideBarComponent />
+        </div>
+        <section>
+            <RouterView />
+        </section>
+    </main>
 </template>
+<style lang="scss" scoped>
+main {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+
+    .aside-space {
+        width: 20%;
+        height: 100vh;
+    }
+}
+</style>
