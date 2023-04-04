e192a7759c414cbb8e864ffd4b88f6f600d3cade
RaulMartinezF5 <raulmartinezalvarez1980@gmail.com>
2023-04-04 10:43:02 +0200

    complete merge

--- src/main/frontend/src/App.vue
+++ src/main/frontend/src/App.vue
@@ -2,22 +2,7 @@
 import { RouterLink, RouterView } from "vue-router";
 </script>
 <template>
-   <header>
-    <!-- <nav>
-      <RouterLink to="/">Home</RouterLink> 
-  <header>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
 
-      <RouterLink to="/about">About</RouterLink>
-    </nav> -->
-  </header>
-
-
- <!-- <CardHistorial/> -->
-
-
-  <main>
   
     <RouterView />
 
