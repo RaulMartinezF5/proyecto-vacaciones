df669582c43149e4bcedbd1f3bbac3ad9a95f00c
Andrespz07 <andresperezortega2001@gmail.com>
2023-04-04 12:41:30 +0200

    limpiando LoginView

--- src/main/frontend/src/views/LoginView.vue
+++ src/main/frontend/src/views/LoginView.vue
@@ -1,14 +1,12 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router';
 import Login from "../components/Login.vue";
-import UserListComponent from "../components/UserListComponent.vue";
 import { defineProps } from "vue";
 
 </script>
 
 <template>
-  <!-- <Login /> -->
-<UserListComponent></UserListComponent>
+<Login />
 
 </template>
 
