143061e4f16fa9e9c131d88f8a69100220a428af
blake S <design.harls@icloud.com>
2023-04-05 14:36:13 +0200

    card finalizada de historial de solicitudes

--- src/main/frontend/src/views/Admin/RequestListViewAdmin.vue
+++ src/main/frontend/src/views/Admin/RequestListViewAdmin.vue
@@ -1,19 +1,12 @@
 <script setup>
-import AsideBarComponent from "../../components/AsideBarComponent.vue";
-import Header from "../../components/Header.vue"
+import CardHistorial from '../../components/CardHistorial.vue';
 
 </script>
 <template>
-<<<<<<< HEAD
     <div>
-        
-      
-
-        
+        <CardHistorial/>
     </div>
-=======
-<h1>requestListView</h1>
->>>>>>> 23b3cbd8af16ef0c22ccb495beb6fd9fe8aedcae
+    
 </template>
 <style lang="scss" scoped> 
 </style>
