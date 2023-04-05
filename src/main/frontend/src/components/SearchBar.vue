2823daa2dfde953073043edc59e94f0e465e7a11
Andrespz07 <andresperezortega2001@gmail.com>
2023-04-05 10:40:08 +0200

    UserListView maquetada

--- src/main/frontend/src/components/SearchBar.vue
+++ src/main/frontend/src/components/SearchBar.vue
@@ -5,7 +5,7 @@
 <template>
    <v-form width="900" class="d-flex flex-row justify-center mb-7">
    <input type="text" v-model="searchTerm">
-        <v-btn @click="validate" class="v-btn">
+        <v-btn @click="validate" class="v-btn" id="button">
           BUSCAR
         </v-btn>
 </v-form> 
@@ -21,15 +21,13 @@ height: 2vw;
 border-color: map-get($map: c.$colors, $key: "Black");
 border: 2px solid;
 margin-right: 1vw;
+}
 
-.v-btn{
-   width: 10vw;
+#button{
    background-color: map-get($map: c.$colors, $key: "Orange");
     color:white;
     border-radius: 10px;
-    margin-left: 1vh;
-    font-weight: 700;
-  
-}}
+    font-weight: 700;  
+}
 
 </style>
