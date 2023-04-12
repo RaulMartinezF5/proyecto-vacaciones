1c56e59ba11a516b9ff8646ab717d128a67a3669
guillermo <h.uhm292@gmail.com>
2023-04-12 12:40:18 +0200

    graficsView-finish

--- src/main/frontend/src/views/Admin/StatisticsView.vue
+++ src/main/frontend/src/views/Admin/StatisticsView.vue
@@ -1,91 +1,112 @@
 <script setup>
 import iconArrowLeft from '@/assets/img/iconArrowLeft.png';
-import rechazada from '../../components/Rechazada.vue';
+import ButtonComponent from '../../components/ButtonComponent.vue';
 
 </script>
 
 <template>
-<div class="mainContainer">
-        <div class="header">
-            <div class="microheader">
-                <button class="button-arrow"><img :src="iconArrowLeft" alt="" class="arrow"></button>
-                <h1 class="Titulo">
-                    INFORMES ESTADISTICOS
-                </h1>
-            </div>
+    <div class="mainContainer">
+        <!-- <div class="header"> -->
+        <div class="microheader">
+            <button class="button-arrow"><img :src="iconArrowLeft" alt="" class="arrow"></button>
+            <h1 class="Titulo">
+                INFORMES ESTADISTICOS
+            </h1>
+        </div>
 
-            <div class="buttons">
-                <rechazada />
-                <rechazada />
-                <rechazada />
-             </div>
+        <div class="buttons">
+            <ButtonComponent :button="'Escuela'" class="buttonFix"/>
+            <ButtonComponent :button="'Puesto'" class="buttonFix"/>
+            <ButtonComponent :button="'Estado'" class="buttonFix"/>
+        </div>
 
-             <div class="container">
+        <div class="container">
+            <div class="pepin">
                 <div class="grafic"></div>
                 <div class="grafic2"></div>
+            </div>
+            <div class="pepon">
                 <div class="grafic3"></div>
-             </div>
-
             </div>
 
         </div>
-        
-        
-    </template>
+            
+
+    </div>
+</template>
 
 
 <style scoped lang="scss">
-
 .mainContainer {
     width: 100%;
     display: flex;
-    justify-content: space-around;
-    margin: 2% 5%;
-}
-.buttons{
-display: flex;
-flex-direction: row;
-justify-content: space-between;
-}
-.container{
-    display: flex;
     flex-direction: column;
-    height: 100%;
+    justify-content: space-between;
+    padding: 2% 5%;
+
+    .microheader {
+        display: flex;
+        justify-content: center;
+        .button-arrow{
+            position: absolute;
+            left: 24%;
+        }
+    }
+    .buttons {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        width: 100%;
+        margin-top: 2%;
+        .buttonFix {
+            width: 25%;
+        }
+    }
+    .container {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        height: 100%;
+        width: 100%;
+        margin-top: 2%;
+        
+        .pepin{
+            display: flex;
+            flex-direction: column;
+            height: 100%;
+            width: 48%;
+        
+            .grafic {
+                height: 180px;
+                background-color: red;
+                margin-top: 2%;
+                margin-left: 0%;
+                width: 100%;
+            }
+            
+            .grafic2 {
+                height: 180px;
+                background-color: black;
+                margin-top: 2%;
+                margin-left: 0%;
+                width: 100%;
+            }
+        }
+        .pepon{
+                display: flex;
+                width: 48%;
+                height: 100%;
+    
+            .grafic3 {
+                    height: 366px;
+                    background-color: aqua;
+                    margin-top: 2%;
+                    margin-left: 0;
+                    width: 100%;
+                }
+            }
+    }
 }
-.grafic{
-    display: flex;
-    background-color: red;
-    margin-top: 20%;
-}
-.grafic2{
-    background-color: black;
-    margin-top: 20%;
-    margin-left: 70%;
-}
-.grafic3{
-    background-color: aqua;
-    margin-top: 20%;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
