ffa7af8783d87403abe1fbab3377762c87208599
David Daza <drdaza18@gmail.com>
2023-04-27 00:36:11 +0200

    login component

--- src/main/frontend/src/components/Login.vue
+++ src/main/frontend/src/components/Login.vue
@@ -2,38 +2,48 @@
 import { ref } from 'vue';
 import { useAuthStore } from '../stores/authStore';
 import { useRouter } from 'vue-router';
-
 const router = useRouter();
-const authStore = useAuthStore()
-
-const username = ref('')
-const password = ref('')
-
-const login = async(username, password)=>{
-  const response = await authStore.login(username, password)
-  
-  if(response == 'auth') {
-
-    router.push({name: 'LayoutSession'})
+const authStore = useAuthStore();
+const username = ref('');
+const password = ref('');
+const error = ref('');
+const login = async () => {
+  if (!username.value || !password.value) {
+  } else {
+    const response = await authStore.login(username.value, password.value);
+    if (response === 'auth') {
+      router.push({ name: 'LayoutSession' });
+    } else if (response === 'user-not-found') {
+      error.value = 'Usuario no encontrado';
+    } else if (response === 'password-not-found') {
+      error.value = 'Contraseña incorrecta';
+    } else {
+      error.value = 'Ocurrió un error al iniciar sesión';
+    }
   }
-}
-
-
+};
+const nameRules = [
+  value => !!value || 'Campo requerido.',
+];
+const passwordRules = [
+  value => !!value || 'Campo requerido.',
+];
 </script>
 <template>
   <v-sheet width="37%" class="form mx-auto">
     <v-img :width="'50%'" aspect-ratio="16/9" cover src="../../src/assets/img/logo-login.jpg" class="img"></v-img>
-    <v-form ref="form" @keyup.enter="login(username, password)">
-      <v-text-field color="#FF4700" v-model="username" :counter="10" :rules="nameRules" label="Usuario" required
+    <v-form ref="form" @submit.prevent="login">
+      <v-text-field color="#FF4700" v-model="username" :rules="nameRules" label="Usuario" required
         class="form__name"></v-text-field>
-
-      <v-text-field v-model="password" color="#FF4700" :counter="10" :rules="passwordRules" label="Contraseña" required
+      <v-text-field v-model="password" color="#FF4700" :rules="passwordRules" label="Contraseña" required
         class="form__pass" type="password"></v-text-field>
-
       <div class="btn">
-        <v-btn color="#FF4700" rounded="xs" :width="'50%'"  @click="login(username,password)">
+        <v-btn color="#FF4700" rounded="xs" :width="'50%'" type="submit">
           Iniciar sesión
         </v-btn>
+      </div>
+      <div v-if="error" class="error">
+        {{ error }}
       </div>
     </v-form>
   </v-sheet>
@@ -42,24 +52,19 @@ const login = async(username, password)=>{
 .form {
   box-shadow: 1px 3px 10px 1px rgba(0, 0, 0, 0.48);
   border-radius: 12px;
-
   .img {
     display: flex;
     max-width: 100%;
     position: relative;
     margin: auto;
-
   }
-
   .form__name {
     margin: 7%;
     border-radius: 12px;
   }
-
   .form__pass {
     margin: 7%;
   }
-
   .btn {
     display: flex;
     justify-content: center;
